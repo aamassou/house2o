ec1eb4b3673fbae59e346825809602a7d8a5ada2
Yusri Ariff <yusriariff7@gmail.com>
2020-09-05 08:49:51 +0800

    feat: Committing to submit changes to waterfall module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div id="waterfall"></div>
+      <div id="waterfall">
+        <div id="waterfallContainer">
+          <p id="headerTitle">House2o</p>
+          <p id="headerSubtitle">Lorem Ipsum Dolor etc Lorem Ipsum Dolor etc</p>
+        </div>
+
+        <div id="waterfallContainer" style={{ marginTop: 40 }}>
+          <p id="headerTitle">House2o</p>
+          <p id="headerSubtitle">Lorem Ipsum Dolor etc Lorem Ipsum Dolor etc</p>
+        </div>
+      </div>
       <div id="house-container">
         <div id="house-background">
           <div></div>
