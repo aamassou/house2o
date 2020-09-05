167a386f25494e3878e679c437602e1f389902c0
Yusri Ariff <yusriariff7@gmail.com>
2020-09-05 11:07:58 +0800

    fix: Commenting and changing white to HEXcode value

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <div id="waterfall">
+
+        {/* Template for titles/subtitle */}
         <div id="waterfallContainer">
           <p id="headerTitle">House2o</p>
           <p id="headerSubtitle">Lorem Ipsum Dolor etc Lorem Ipsum Dolor etc</p>
